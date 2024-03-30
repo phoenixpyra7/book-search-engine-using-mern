4e6dc0197d8b86d87a024ac1671ce32822078f9d
Seven <phoenixpyra7@gmail.com>
2024-03-30 11:32:14 -0500

    installed dep again

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,11 +15,25 @@
       throw AuthenticationError;
     },
   },
-  // Mutation: {
-  //   addName: async () => {
-  //     return "Name Added";
-  //   },
-  // },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw AuthenticationError;
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const token = signToken(user);
+      return { token, user };
+    }
+  },
 };
 
 module.exports = resolvers;
