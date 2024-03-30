a3562867153c7aafff8633073f7b8afd86c106bd
Seven <phoenixpyra7@gmail.com>
2024-03-30 10:22:31 -0500

    removed duplicate server call and middlewares.

--- server/server.js
+++ server/server.js
@@ -17,17 +17,17 @@ const server = new ApolloServer({
 });
 
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
